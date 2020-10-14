92b29f5a27d38606b273fce88b598015cfeecc67
jacquess1990 <69588190+jacquess1990@users.noreply.github.com>
2020-10-14 20:27:28 +0200

    Few Corrections
    Step by step editing ActivityFourth

--- app/src/main/java/javathlete/example/weightlifter/ActivityFourth.java
+++ app/src/main/java/javathlete/example/weightlifter/ActivityFourth.java
@@ -17,6 +17,18 @@ public class ActivityFourth extends AppCompatActivity {
     private EditText editTextLiftedWeight;
     private EditText editNumberReps;
     private TextView RmOP;
+    private EditText editTextNumberSquatWeight;
+    private EditText editTextNumberSquatReps;
+    private Button weightLiftedSquat;
+    private TextView textViewSquatOP;
+    private EditText editTextNumberLyingBenchRow;
+    private EditText editTextNumberLBRReps;
+    private Button weightLiftedLBR;
+    private TextView textViewLBROP;
+    private EditText benchPressWeight;
+    private EditText benchPressWeightReps;
+    private Button weightLiftedBenchPress;
+    private TextView textViewBPOP;
 
     public static final String TAG = "Fourth started";
 
@@ -34,10 +46,48 @@ public class ActivityFourth extends AppCompatActivity {
         editNumberReps = (EditText) findViewById(R.id.editNumberReps);
         RmOP = (TextView) findViewById(R.id.textViewRmOP);
 
+        editTextNumberSquatWeight = (EditText) findViewById(R.id.editTextNumberSquatWeight);
+        editTextNumberSquatReps = (EditText) findViewById(R.id.editTextNumberSquatReps);
+        weightLiftedSquat = (Button) findViewById(R.id.weightLiftedSquat);
+        textViewSquatOP = (TextView) findViewById(R.id.textViewSquatOP);
+
+        editTextNumberLyingBenchRow = (EditText) findViewById(R.id.editTextNumberLyingBenchRow);
+        editTextNumberLBRReps = (EditText) findViewById(R.id.editTextNumberLBRReps);
+        weightLiftedLBR = (Button) findViewById(R.id.weightLiftedLBR);
+        textViewLBROP = (TextView) findViewById(R.id.textViewRmLBROP);
+
+        benchPressWeight = (EditText) findViewById(R.id.benchPressWeight);
+        benchPressWeightReps = (EditText) findViewById(R.id.benchPressWeightReps);
+        weightLiftedBenchPress = (Button) findViewById(R.id.weightLiftedBenchPress);
+        textViewBPOP = (TextView) findViewById(R.id.textViewBPOP);
+
+
+
         weightLifted.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 editTextViewParse();
+            }
+        });
+
+        weightLiftedSquat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextViewParseSquat();
+            }
+        });
+
+        weightLiftedLBR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextViewParseLBR();
+            }
+        });
+
+        weightLiftedBenchPress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editTextViewParseBP();
             }
         });
 
@@ -55,13 +105,33 @@ public class ActivityFourth extends AppCompatActivity {
     }
 
     public void editTextViewParseSquat  () {
-        String firstEditValue = editTextLiftedWeight.getText().toString();
-        String secondEditValue = editNumberReps.getText().toString();
-        double myFirstEditValue = Double.parseDouble(firstEditValue);
-        double mySecondEditValue = Double.parseDouble(secondEditValue);
-        double finalOutcome = myFirstEditValue / (1.0278 - 0.0278 * mySecondEditValue);
-        String finalOutcome1 = String.valueOf(finalOutcome);
-        RmOP.setText(finalOutcome1);
+        String firstEditValueSq = editTextNumberSquatWeight.getText().toString();
+        String secondEditValueSq = editTextNumberSquatReps.getText().toString();
+        double myFirstEditValueSq = Double.parseDouble(firstEditValueSq);
+        double mySecondEditValueSq = Double.parseDouble(secondEditValueSq);
+        double finalOutcomeSq = myFirstEditValueSq / (1.0278 - 0.0278 * mySecondEditValueSq);
+        String finalOutcomeSquat = String.valueOf(finalOutcomeSq);
+        textViewSquatOP.setText(finalOutcomeSquat);
+    }
+
+    public void editTextViewParseLBR () {
+        String firstEditValueLBR = editTextNumberLyingBenchRow.getText().toString();
+        String secondEditValueLBR = editTextNumberLBRReps.getText().toString();
+        double myFirstEditValueLBR = Double.parseDouble(firstEditValueLBR);
+        double mySecondEditValueLBR = Double.parseDouble(secondEditValueLBR);
+        double finalOutcomeLBR = myFirstEditValueLBR / (1.0278 - 0.0278 * mySecondEditValueLBR);
+        String finalOutcomeLyingBenchRow = String.valueOf(finalOutcomeLBR);
+        textViewLBROP.setText(finalOutcomeLyingBenchRow);
+    }
+
+    public void editTextViewParseBP () {
+        String firstEditValueBP = benchPressWeight.getText().toString();
+        String secondEditValueBP = benchPressWeightReps.getText().toString();
+        double myFirstEditValueBP = Double.parseDouble(firstEditValueBP);
+        double mySecondEditValueBP = Double.parseDouble(secondEditValueBP);
+        double finalOutcomeBP = myFirstEditValueBP / (1.0278 - 0.0278 * mySecondEditValueBP);
+        String finalOutcomeBenchPress = String.valueOf(finalOutcomeBP);
+        textViewBPOP.setText(finalOutcomeBenchPress);
     }
 
 }
